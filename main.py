da998f7bca2933168565347bcf57940a48da582e
Evgeniy Sevostyanihin <sevostyanikhin02@gmail.com>
2023-12-01 05:10:32 +0300

    threads

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ def fix_words(segments: list[dict]):
 
 
 def run_subtitle(sub, i, frame, out_queue):
-    out_queue.put((i, sub(i, frame)))
+    frame = sub(i, frame)
+    out_queue.put((i, frame))
 
 
 def queue_thread(q, subtitle_stream, out_queue):
@@ -34,7 +35,45 @@ def queue_thread(q, subtitle_stream, out_queue):
         if sub is None:
             out_queue.put((i, frame))
         else:
-            threading.Thread(target=run_subtitle, args=(sub, i, frame, out_queue,)).start()
+            threading.Thread(target=run_subtitle, args=(sub, i, frame, out_queue,), daemon=True).start()
+
+
+def write_thread(output_video, out_queue: queue.Queue, length):
+    i = 0
+    out_frames = []
+    while True:
+        i_, frame = out_queue.get()
+
+        if i_ == i:
+            output_video.write(frame)
+            del frame
+            i += 1
+        else:
+            out_frames.append((i_, frame))
+
+        if len(out_frames) > 0 and not len(out_frames) % 50:
+            out_frames = sorted(out_frames, key=lambda x: x[0], reverse=False)
+
+        while len(out_frames) and out_frames[0][0] == i:
+            j, frame = out_frames.pop(0)
+            output_video.write(frame)
+            del frame
+            i += 1
+
+        if i >= length:
+            break
+
+        # if out_frames[0][0] == i:
+        #     for last_i, (j, frame) in enumerate(out_frames):
+        #         if j == i:
+        #             output_video.write(frame)
+        #             i += 1
+        #         else:
+        #             break
+        
+        #     print(len(out_frames), last_i)
+        #     for _ in range(last_i + 1):
+        #         del out_frames[0]
 
 
 def render_subtitles(
@@ -46,7 +85,7 @@ def render_subtitles(
     width = int(vid_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(vid_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = vid_capture.get(cv2.CAP_PROP_FPS)
-
+    length = int(vid_capture.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
     output_video = cv2.VideoWriter(
         video_output_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (width, height)
     )
@@ -59,29 +98,23 @@ def render_subtitles(
 
     main_queue = queue.Queue()
     out_queue = queue.Queue()
-    main_threads = [
+    [
         threading.Thread(target=queue_thread, args=(main_queue, subtitle_stream, out_queue), daemon=True).start()
         for _ in range(12)
     ]
+
+    threading.Thread(target=write_thread, args=(output_video, out_queue, length, ), daemon=True).start()
 
     while vid_capture.isOpened():
         ret, frame = vid_capture.read()
         if ret:
             main_queue.put((i, frame))
             i += 1
+            if main_queue.qsize() > 500:
+                print("SLEEP", main_queue.qsize())
+                time.sleep(0.1)
         else:
             break
-
-    out_frames = []
-
-    while len(out_frames) < i:
-        inp = out_queue.get()
-        out_frames.append(inp)
-
-    out_frames = sorted(out_frames, key=lambda x: x[0], reverse=True)
-
-    for _, frame in out_frames:
-        output_video.write(frame)
 
 
 if __name__ == '__main__':
@@ -89,15 +122,19 @@ if __name__ == '__main__':
     import json
     import time
 
-    test_video_path = "test_video/IMG_2073.MOV"
-    out_video_path = "test_video/test_2s56.mp4"
-    result_path = "test_video/test_2.json"
+    # test_video_path = "test_video/IMG_2073.MOV"
+    # out_video_path = "test_video/test_2s5116.mp4"
+    # result_path = "test_video/test_2.json"
+
+    test_video_path = "/home/jjjj/Downloads/Telegram Desktop/Forming the band with Noel Redding and Mitch Mitchell_.mp4"
+    out_video_path = "test_video/test_23ss.mp4"
+    result_path = "/home/jjjj/Documents/render_subtitles/123sdkalads.json"
     with open(result_path, "r") as t:
         result = json.load(t)
 
     preset = THEMES["HORMOZI 3"]
     start_time = time.time()
     render_subtitles(
-        result["data"]["segments"], test_video_path, out_video_path, preset
+        result["segments"], test_video_path, out_video_path, preset
     )
     print(time.time() - start_time)
